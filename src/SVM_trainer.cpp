84d858f56079b1d99e485a22ca4994b000ec4d74
ameyawagh <ameya555@gmail.com>
2018-02-08 14:57:31 -0500

    SVM_trainer node made modular

--- src/SVM_trainer.cpp
+++ src/SVM_trainer.cpp
@@ -27,8 +27,6 @@
 
 class getPointCloud{
     private :
-        int b;
-        bool fileStatus;
         struct stat info;
     public :
         std::string PCD_BASE_PATH;
@@ -42,125 +40,134 @@ class getPointCloud{
         pcl::PointCloud<pcl::VFHSignature308>::Ptr VFH_descriptor;
 
         ofstream outputFile;
-        int a;
+
         processPointCloud pc;
         classifier clf;
 
-    getPointCloud(std::string _PCD_BASE_PATH,
-                  std::vector<std::string> _DataFiles,
-                  std::string _CLASSIFIER_NAME): cloud(new pcl::PointCloud<pcl::PointXYZ>),
-                         VFH_descriptor(new pcl::PointCloud<pcl::VFHSignature308>),
-                         clf()
-    {
-        a=2;
-        b=3;
-        fileStatus = false;
+        getPointCloud(std::string _PCD_BASE_PATH,
+                      std::vector<std::string> _DataFiles,
+                      std::string _CLASSIFIER_NAME);
 
-        PCD_BASE_PATH = _PCD_BASE_PATH;
-        CLASSIFIER_NAME = _CLASSIFIER_NAME;
-        for(size_t i=0;i<_DataFiles.size();i++){
-            DataFiles.push_back(PCD_BASE_PATH +_DataFiles[i]);
-        }
-
-    }
-
-    pcl::PointCloud<pcl::PointXYZ>::Ptr loadCloud(std::string filePath){
-        pcl::PointCloud<pcl::PointXYZ>::Ptr someCloud (new pcl::PointCloud<pcl::PointXYZ>);
-        if (pcl::io::loadPCDFile<pcl::PointXYZ> (filePath, *someCloud) == -1) //* load the file
-        {
-          PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
-        }
-
-        return someCloud;
-    }
-
-
-    void getTrainingData(cv::Mat& trainingData,cv::Mat& trainingLabels){
-        float minX,minY,minZ=1000.0;
-        float maxX,maxY,maxZ=0.0;
-        pcl::VoxelGrid<pcl::PointXYZ> vg;
-
-        cv::Mat label = cv::Mat::zeros(1,1,CV_32FC1);
-        for(size_t fileIdx=0; fileIdx<DataFiles.size();fileIdx++){
-
-            //--------------------------------------//
-            std::vector<std::string> files = std::vector<std::string>();
-
-            getdir(DataFiles[fileIdx]+"/",files);
-            //--------------------------------------//
-
-            cout << "no of files" << files.size() << endl;
-            //--------------------------------------//
-            pcl::PointXYZ minPt, maxPt;
-
-
-            int skip=0;
-            for (unsigned int i = 0;i < files.size();i++) {
-
-//                 remove this after testing classifier training
-//                if(skip > 5){break;}
-//                skip++;
-
-                if(files[i] == "."  || files[i] == ".."){
-                    cout << "ignore" << endl;
-                }
-                else{
-                    std::cout << " [ processing ] " << files[i];
-
-                    cloud = loadCloud(DataFiles[fileIdx]+"/"+files[i]);
-
-                    //-- voxelization of the cloud --------//
-                    vg.setInputCloud (cloud);
-                    vg.setLeafSize (0.01f, 0.01f, 0.01f);
-                    vg.filter (*cloud);
-                    //-------------------------------------//
-
-
-                    pc.cloudinput(cloud);
-                    VFH_descriptor = pc.getDescriptor();
-                    cv::Mat _descriptor = cv::Mat::zeros(1,306,CV_32FC1);
-
-                    for(size_t i=0;i<306;i++){
-                        _descriptor.at<float>(0,i)=(float)VFH_descriptor->points[0].histogram[i];
-                    }
-                    trainingData.push_back(_descriptor);
-                    if(DataFiles[fileIdx] == (PCD_BASE_PATH+CLASSIFIER_NAME)){
-                        label.at<float>(0,0)=1.0;
-                        trainingLabels.push_back(label);
-                        std::cout << " [label] 1.0";
-                    }
-                    else{
-                        label.at<float>(0,0)= -1.0;
-                        trainingLabels.push_back(label);
-                        std::cout << " [label] -1.0";
-                    }
-                    std::cout << std::endl;
-
-
-                }
-            }
-        }
-    }
-
-
-
-    int getdir (std::string dir, std::vector<std::string> &files)
-    {
-        DIR *dp;
-        struct dirent *dirp;
-        if((dp  = opendir(dir.c_str())) == NULL) {
-            std::cout << "Error(" << errno << ") opening " << dir << endl;
-            return errno;
-        }
-
-        while ((dirp = readdir(dp)) != NULL) {
-            files.push_back(std::string(dirp->d_name));
-        }
-        closedir(dp);
-        return 0;
-    }
+        pcl::PointCloud<pcl::PointXYZ>::Ptr loadCloud(std::string filePath);
+        void getTrainingData(cv::Mat& trainingData,cv::Mat& trainingLabels);
+        int getdir (std::string dir, std::vector<std::string> &files);
 
 };
+
+/*
+ * Constructor
+ */
+getPointCloud::getPointCloud(std::string _PCD_BASE_PATH,
+              std::vector<std::string> _DataFiles,
+              std::string _CLASSIFIER_NAME): cloud(new pcl::PointCloud<pcl::PointXYZ>),
+                     VFH_descriptor(new pcl::PointCloud<pcl::VFHSignature308>),
+                     clf()
+{
+
+
+    PCD_BASE_PATH = _PCD_BASE_PATH;
+    CLASSIFIER_NAME = _CLASSIFIER_NAME;
+    for(size_t i=0;i<_DataFiles.size();i++){
+        DataFiles.push_back(PCD_BASE_PATH +_DataFiles[i]);
+    }
+
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr
+getPointCloud::loadCloud(std::string filePath){
+    pcl::PointCloud<pcl::PointXYZ>::Ptr someCloud (new pcl::PointCloud<pcl::PointXYZ>);
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (filePath, *someCloud) == -1) //* load the file
+    {
+      PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+    }
+
+    return someCloud;
+}
+
+
+void
+getPointCloud::getTrainingData(cv::Mat& trainingData,cv::Mat& trainingLabels){
+    float minX,minY,minZ=1000.0;
+    float maxX,maxY,maxZ=0.0;
+
+    pcl::VoxelGrid<pcl::PointXYZ> vg;
+
+    cv::Mat label = cv::Mat::zeros(1,1,CV_32FC1);
+
+//         Loop over each Directory
+    for(size_t fileIdx=0; fileIdx<DataFiles.size();fileIdx++){
+        std::vector<std::string> files = std::vector<std::string>();
+        pcl::PointXYZ minPt, maxPt;
+        int skip=0;
+
+        getdir(DataFiles[fileIdx]+"/",files);
+        ROS_INFO("No of Files: %d",(int)files.size());
+
+        //    cv::Mat trainingData;
+        //    cv::Mat trainingLabels;
+        // Loop over each file
+        for (unsigned int i = 0;i < files.size();i++) {
+//                 remove this after testing classifier training
+            if(skip > 5){break;}
+            skip++;
+
+            if(files[i] == "."  || files[i] == ".."){
+                ROS_INFO(" . or .. files ignored");
+            }
+            else{
+                std::cout << " [ processing ] " << files[i];
+
+                cloud = loadCloud(DataFiles[fileIdx]+"/"+files[i]);
+
+                //-- voxelization of the cloud --------//
+                vg.setInputCloud (cloud);
+                vg.setLeafSize (0.01f, 0.01f, 0.01f);
+                vg.filter (*cloud);
+                //-- Getting the descriptors ----------//
+                pc.cloudinput(cloud);
+                VFH_descriptor = pc.getDescriptor();
+                cv::Mat _descriptor = cv::Mat::zeros(1,306,CV_32FC1);
+
+                for(size_t i=0;i<306;i++){
+                    _descriptor.at<float>(0,i)=(float)VFH_descriptor->points[0].histogram[i];
+                }
+                //-------------------------------------//
+                trainingData.push_back(_descriptor);
+                if(DataFiles[fileIdx] == (PCD_BASE_PATH+CLASSIFIER_NAME)){
+                    label.at<float>(0,0)=1.0;
+                    trainingLabels.push_back(label);
+                    std::cout << " [label] 1.0";
+                }
+                else{
+                    label.at<float>(0,0)= -1.0;
+                    trainingLabels.push_back(label);
+                    std::cout << " [label] -1.0";
+                }
+                std::cout << std::endl;
+
+            }
+        }
+        // here
+    }
+}
+
+
+int
+getPointCloud::getdir (std::string dir, std::vector<std::string> &files)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dir.c_str())) == NULL) {
+        std::cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        files.push_back(std::string(dirp->d_name));
+    }
+    closedir(dp);
+    return 0;
+}
 
 int main(int argc, char** argv){
     ros::init(argc,argv,"SVMTrainer");
@@ -196,27 +203,23 @@ int main(int argc, char** argv){
         getPointCloud p(PCD_BASE_PATH,PCD_CLASS_PATHS,POSITIVE_CLASS);
         p.getTrainingData(trainingData,trainingLabels);
 
-        std::cout << trainingData.depth() << std::endl;
-        std::cout << trainingLabels.depth() << std::endl;
-        std::cout << "rows:" << trainingData.rows << " cols:"<< trainingData.cols << std::endl;
-        std::cout << "rows:" << trainingLabels.rows << " cols:" << trainingLabels.cols << std::endl;
+
+        ROS_INFO("Training Data dimensions: rows: %d cols: %d",(int)trainingData.rows,(int)trainingData.cols);
+        ROS_INFO("Training Labels dimensions: rows: %d cols: %d",(int)trainingLabels.rows,(int)trainingLabels.cols);
 
         {
-
             cv::FileStorage file(trainDataFile, cv::FileStorage::WRITE);
             file << "trainingData" << trainingData;
             file.release();
-
         }
-
         {
 
             cv::FileStorage file(trainLabelsFile, cv::FileStorage::WRITE);
             file << "trainingLabels" << trainingLabels;
             file.release();
         }
-
-        std::cout << "training Data saved" << std::endl;
+//        std::cout << "training Data saved" << std::endl;
+        ROS_INFO("Training Data saved.");
     }
 
     if(trainSVM){
@@ -233,12 +236,13 @@ int main(int argc, char** argv){
             file.release();
         }
 
-        std::cout << "Files loaded" << std::endl;
-        std::cout << "training SVM" << std::endl;
+        ROS_INFO("Files Loaded.");
+        ROS_INFO("training SVM.");
         clf.trainSVM(trainingData,trainingLabels,classifierPath);
-        std::cout << "trained SVM" << std::endl;
+        ROS_INFO("SVM trained.");
+
     }
 
-//    ros::spin();
+    ros::shutdown();
     return 0;
 }
